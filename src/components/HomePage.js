bf7667a3eb415aa02173146675d27e33a630505a
Lee Cant <leerobertcant@gmail.com>
2019-07-07 20:50:28 +0100

    Dependency updates

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//React component instantiated by function
+function HomePage() {
+  return;
+  <div className="jumbotron">
+    <h1>Some title</h1>
+    <p>A random paragraph here.</p>
+  </div>;
+}
+
+export default HomePage;
